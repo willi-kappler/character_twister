71685da8a89b3842bb3ea6318d42593d4f972e47
Willi Kappler <willi-kappler@users.noreply.github.com>
2016-09-23 23:41:59 +0200

    work on configuration

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,18 @@
-extern crate clap;
 use clap::{Arg, App};
+use yaml_rust::YamlLoader;
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum ConfigurationError {
+        InvalidFontRange {}
+        FontPathDoesNotExist {}
+        InputPathDoesNotExist {}
+        ErrorInConfigFile {}
+    }
+}
 
 #[derive(Clone,Debug)]
-struct Configuration {
+pub struct Configuration {
     input_path: String,
     font_size_min: u8,
     font_size_max: u8,
@@ -10,7 +20,33 @@ struct Configuration {
 
 }
 
-pub fn create_config() -> Configuration {
+fn load_config_file(file_name: String) -> Result<Configuration, ConfigurationError> {
+    Ok(default_configuration())
+}
+
+fn default_configuration() -> Configuration {
+    Configuration {
+        input_path: "./".to_string(),
+        font_size_min: 10,
+        font_size_max: 10,
+        font_path: "/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf".to_string()
+    }
+}
+
+fn parse_font_size(font_size: &str) -> Result<(u8, u8), ConfigurationError> {
+    let font_size_min = 10;
+    let font_size_max = 10;
+
+    // Valid arguments:
+    // 10
+    // 10,15
+    // 10-15
+    // 10;15
+
+    Ok((font_size_min, font_size_max))
+}
+
+pub fn create_config() -> Result<Configuration, ConfigurationError> {
     let version = "0.1";
 
     let matches = App::new("Character Twister")
@@ -28,21 +64,38 @@ pub fn create_config() -> Configuration {
         .arg(Arg::with_name("version")
             .short("v")
             .long("version")
-            .help("Shows version number")
+            .help("Shows version number"))
         .arg(Arg::with_name("font_size")
             .short("s")
             .long("size")
-            .help("Specify the range of font sizes")
+            .help("Specify the range of font sizes"))
         .arg(Arg::with_name("font_path")
             .short("f")
             .long("font")
-            .help("Specify the font path")
+            .help("Specify the font path"))
         .get_matches();
 
-        if let Some(config_file) = matches.value_of("config") {
-            println!("Loading configuration file: {}", config_file);
-        }
+    let mut configuration = default_configuration();
 
-        // Command line parameter can overwrite configuration file settings
+    if let Some(config_file_name) = matches.value_of("config") {
+        println!("Loading configuration file: {}", config_file_name);
+        configuration = try!(load_config_file(config_file_name.to_string()));
+    }
 
+    // Command line parameter can overwrite configuration file settings
+
+    // Save to call unwrap() here since "input" is mandatory
+    configuration.input_path = matches.value_of("input").unwrap().to_string();
+
+    if let Some(font_size) = matches.value_of("font_size") {
+        let (font_size_min, font_size_max) = try!(parse_font_size(font_size));
+        configuration.font_size_min = font_size_min;
+        configuration.font_size_max = font_size_max;
+    }
+
+    if let Some(font_path) = matches.value_of("font_path") {
+        configuration.font_path = font_path.to_string();
+    }
+
+    Ok(configuration)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+#[macro_use] extern crate quick_error;
 extern crate darwin_rs;
-
-use darwin_rs::{Individual, SimulationBuilder, Population, PopulationBuilder, SimError};
+extern crate clap;
+extern crate yaml_rust;
 
 mod config;
 
-use config::{Configuration, create_config};
+use darwin_rs::{Individual, SimulationBuilder, Population, PopulationBuilder, SimError};
 
+use config::{Configuration, create_config};
 
 fn main() {
     let config = create_config();
 
-    
+
 
 }
