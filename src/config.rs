95721cd4f5830fb3322a00f8a384156f900a0f83
Willi Kappler <willi.kappler.gm@gmail.com>
2017-06-26 17:47:02 +0200

    use load_font, default_config as function, refactor

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,21 @@
 use clap::{Arg, App};
 
-#[derive(Clone,Debug)]
+#[derive(Clone, Debug)]
 pub struct Configuration {
-    input_path: String,
-    font_size_min: u8,
-    font_size_max: u8,
-    font_path: String,
+    pub input_path: String,
+    pub font_size_min: u8,
+    pub font_size_max: u8,
+    pub font_name: String,
 
+}
+
+fn default_config() -> Configuration {
+    Configuration {
+        input_path: String::from("./"),
+        font_size_min: 8,
+        font_size_max: 16,
+        font_name: String::from("FreeMono")
+    }
 }
 
 pub fn create_config() -> Configuration {
@@ -18,10 +27,10 @@ pub fn create_config() -> Configuration {
             .short("c")
             .long("config")
             .value_name("FILE")
-            .help("Sets a configuration file")
+            .help("Sets a configuration file, command line arguments can overwrite values from the config file")
             .takes_value(true))
         .arg(Arg::with_name("input")
-            .help("Sets the input file or folder")
+            .help("Sets the input file or folder, default is current folder './'")
             .index(1))
         .arg(Arg::with_name("version")
             .short("v")
@@ -30,12 +39,12 @@ pub fn create_config() -> Configuration {
         .arg(Arg::with_name("font_range")
             .short("s")
             .long("size")
-            .help("Specify the range of font sizes to try, separated with a colon ':'. ex.: 8:20")
+            .help("Specify the range of font sizes to try, separated with a colon ':'. ex.: 8:16 which is the default")
             .takes_value(true))
-        .arg(Arg::with_name("font_path")
+        .arg(Arg::with_name("font_name")
             .short("f")
             .long("font")
-            .help("Specify the font path")
+            .help("Specify the font to use")
             .takes_value(true))
         .after_help(
             "Examples:\n\
@@ -49,65 +58,62 @@ pub fn create_config() -> Configuration {
         )
         .get_matches();
 
-        // Default values:
-        let mut result = Configuration {
-            input_path: "./".to_string(),
-            font_size_min: 8,
-            font_size_max: 16,
-            font_path: "/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf".to_string()
-        };
+    // Default values:
+    let mut result = default_config().clone();
 
-        if let Some(config_file) = matches.value_of("config") {
-            println!("Loading configuration file: {}", config_file);
-        }
+    if let Some(config_file) = matches.value_of("config") {
+        result = load_config(config_file);
+    }
 
-        // Command line parameter can overwrite configuration file settings
+    // Command line parameter can overwrite configuration file settings
+    if let Some(input_path) = matches.value_of("input") {
+        result.input_path = input_path.to_string();
+        println!("input path: {}", input_path);
+    }
 
-        if let Some(input_path) = matches.value_of("input") {
-            result.input_path = input_path.to_string();
-            println!("input path: {}", input_path);
-        }
+    if let Some(font_name) = matches.value_of("font_name") {
+        result.font_name = font_name.to_string();
+        println!("using font: {}", font_name);
+    }
 
-        if let Some(font_path) = matches.value_of("font_path") {
-            result.font_path = font_path.to_string();
-            println!("using font: {}", font_path);
-        }
+    if let Some(font_range) = matches.value_of("font_range") {
+        let values: Vec<&str> = font_range.split(":").collect();
 
-        if let Some(font_range) = matches.value_of("font_range") {
-            let values: Vec<&str> = font_range.split(":").collect();
+        let num_of_values = values.len();
 
-            let num_of_values = values.len();
+        println!("num_of_values: {}", num_of_values);
 
-            println!("num_of_values: {}", num_of_values);
+        match num_of_values {
+            1 => {
+                println!("wrong format for font range: '{}', use colon ':' to separate values, ex.: '12:16'.", font_range);
+            },
+            2 => {
+                let min_value = values[0].parse::<u8>();
+                let max_value = values[1].parse::<u8>();
 
-            match num_of_values {
-                1 => {
-                    println!("wrong format for font range: '{}', use colon ':' to separate values, ex.: '12:16'.", font_range);
-                },
-                2 => {
-                    let min_value = values[0].parse::<u8>();
-                    let max_value = values[1].parse::<u8>();
-
-                    match (min_value, max_value) {
-                        (Ok(min_value), Ok(max_value)) => {
-                            println!("font range values: {} - {}", min_value, max_value);
-                            result.font_size_min = min_value;
-                            result.font_size_max = max_value;
-                        },
-                        _ => {
-                            println!("wrong format for font range: '{}', two integer values are needed, ex.: '12:16'.", font_range);
-                        }
+                match (min_value, max_value) {
+                    (Ok(min_value), Ok(max_value)) => {
+                        println!("font range values: {} - {}", min_value, max_value);
+                        result.font_size_min = min_value;
+                        result.font_size_max = max_value;
+                    },
+                    _ => {
+                        println!("wrong format for font range: '{}', two integer values are needed, ex.: '12:16'.", font_range);
                     }
-                },
-                _ => {
-                    println!("wrong format for font range: '{}', only two values allowed.", font_range);
                 }
+            },
+            _ => {
+                println!("wrong format for font range: '{}', only two values allowed.", font_range);
             }
         }
-/*
-        if let Some() = matches.value_of("") {
-            result. = ;
-        }
-*/
-        result
+    }
+    result
+}
+
+fn load_config(file_name: &str) -> Configuration {
+    println!("Loading configuration file: {}", file_name);
+
+    let mut result = default_config().clone();
+
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 extern crate darwin_rs;
 extern crate clap;
+extern crate font_loader;
 
 use darwin_rs::{Individual, SimulationBuilder, Population, PopulationBuilder};
 
 mod config;
 
 use config::{create_config};
-
+use font_loader::system_fonts;
 
 fn main() {
     let config = create_config();
 
+/*
+    let sysfonts = system_fonts::query_all();
+
+    for font in &sysfonts {
+        println!("font: {}", font);
+    }
+*/
+
+    let font_property = system_fonts::FontPropertyBuilder::new().
+        family(&config.font_name).build();
+
+    let font = system_fonts::get(&font_property);
+
+    if let Some((font_data, res)) = font {
+        println!("Font found");
+        println!("Font size: {}", font_data.len());
+        println!("res: {}", res);
+    } else {
+        println!("Could find / load font: {}", &config.font_name);
+    }
 }
